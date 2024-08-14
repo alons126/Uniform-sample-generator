5b94df870129a633e68b99f92dd9aa7a5fb9a1a4
alons126 <102910135+alons126@users.noreply.github.com>
2024-08-14 15:52:34 +0300

    first ifarm test

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "- Re-pulling repository -----------------------------------------------"
+echo ""
+#git reset --hard
+git pull
+echo ""
+echo "- Lunching code -------------------------------------------------------"
+echo ""
+root ./CodeRun.cpp -l -q -b
+echo ""
+echo "- Operation finished --------------------------------------------------"
