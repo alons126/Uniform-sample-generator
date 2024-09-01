0629496f43a6f8304d80fb810f4fe79af6293943
alons126 <alonsportes@mail.tau.ac.il>
2024-09-01 12:09:35 +0300

    GEMC tests

--- CodeRun.cpp
+++ CodeRun.cpp
@@ -1,7 +1,8 @@
 #include "Uniform_sample_generator.C"
 
-void CodeRun() {
-    Uniform_sample_generator(true,false,false);
-    Uniform_sample_generator(false,true,false);
-    Uniform_sample_generator(false,false,true);
+void CodeRun()
+{
+    Uniform_sample_generator(true, false, false);
+    Uniform_sample_generator(false, true, false);
+    Uniform_sample_generator(false, false, true);
 }
