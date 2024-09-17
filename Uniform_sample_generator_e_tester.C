01ae689ed6a1b79e1298f631d0ccfb679e7ec93e
alons126 <alonsportes@mail.tau.ac.il>
2024-09-17 08:36:34 +0300

    GEMC and electron acceptance test 2070MeV

---
+++ Uniform_sample_generator_e_tester.C
@@ -0,0 +1,212 @@
+#include <unistd.h>
+#include <iostream>
+
+#include "TList.h"
+#include "TObjString.h"
+
+#if independent_tester
+#include "Generate_uniform_event.C"
+#include "DisplyText.C"
+#include "Histograms.cpp"
+// #include "ConfigBeamE.cpp"
+#include "ConfigPrefix.cpp"
+#include "ConfigTopDir.cpp"
+#endif
+
+/* root CodeRun.cpp -q -b */
+/* LUND format: https://gemc.jlab.org/gemc/html/documentation/generator/lund.html */
+
+// TODO: Ask Andrew which GCARD and YAML files to use!
+// TODO: Talk to Andrew - GENIE to LUND file is outdated!
+
+void Uniform_sample_generator_e_tester(const bool gen_1e_events, const bool gen_ep_events, const bool gen_en_events,
+                                       //   double Ebeam = 5.98636,
+                                       //   double Ebeam = 4.02962,
+                                       double Ebeam = 2.07052,
+                                       //   TString OutPutFolder = "/lustre24/expphy/volatile/clas12/asportes/2N_Analysis_Reco/Uniform_e-p-n_samples/5986MeV/OutPut/",
+                                       //   TString OutPutFolder = "/lustre24/expphy/volatile/clas12/asportes/2N_Analysis_Reco/Uniform_e-p-n_samples/4029MeV/OutPut/",
+                                       TString OutPutFolder = "/lustre24/expphy/volatile/clas12/asportes/2N_Analysis_Reco/Uniform_e-p-n_samples/2070MeV/OutPut_e_tester/",
+                                       // TString OutPutFolder = "./OutPut/",
+                                       // TString OutputFileNamePrefix = "Uniform_sample",
+                                       int nFiles = 10, int nEvents = 10000,
+                                       double theta_e_min = 5., double theta_e_max = 40.,
+                                       double theta_p_min = 5., double theta_p_max = 45.,
+                                       double theta_n_min = 5., double theta_n_max = 35.)
+{
+    // -------------------------------------------------------------------------------------------------
+    cout << "\n\n\n===========================================================================\n";
+    cout << setw(50) << "Uniform sample generator\n";
+    cout << "===========================================================================\n\n";
+
+    TString OutputFileNamePrefix = ConfigPrefix(gen_1e_events, gen_ep_events, gen_en_events, Ebeam);
+    // OutPutFolder = ConfigTopDir(Ebeam); // reconfigure OutPutFolder according to working directory (ifarm or local)
+    OutPutFolder = ConfigTopDir(gen_1e_events, gen_ep_events, gen_en_events, Ebeam, OutPutFolder); // reconfigure OutPutFolder according to working directory (ifarm or local)
+
+    /*if (EnforceMomCon)
+    {
+        OutputFileNamePrefix = "lund_" + OutputFileNamePrefix + "_wMomCon";
+    }
+    else
+    {
+        OutputFileNamePrefix = "lund_" + OutputFileNamePrefix + "_woMomCon";
+    }*/
+
+    bool GenerateLundFiles = true;
+    int DisplaySpace = 74;
+
+    DisplyText("OutputFileNamePrefix", DisplaySpace, OutputFileNamePrefix);
+    DisplyText("nFiles", DisplaySpace, nFiles);
+    DisplyText("nEvents", DisplaySpace, nEvents);
+    DisplyText("Beam energy [GeV]", DisplaySpace, Ebeam);
+
+    DisplyText("GenerateLundFiles", DisplaySpace, GenerateLundFiles);
+
+    cout << "\n";
+
+    TRandom3 ran(0);
+    const int nParticles = 2;
+    double mass_e = 0.511e-3;
+    double mass_p = 0.938272;
+    double mass_n = 0.93957;
+    DisplyText("nParticles", DisplaySpace, nParticles);
+    DisplyText("mass_e [GeV]", DisplaySpace, mass_e);
+    DisplyText("mass_p [GeV]", DisplaySpace, mass_p);
+    DisplyText("mass_n [GeV]", DisplaySpace, mass_n);
+
+    cout << "\n";
+
+    string OutPutFolder0(OutPutFolder.Data());
+    DisplyText("OutPutFolder", DisplaySpace, OutPutFolder);
+
+    TString lundPath = OutPutFolder + "lundfiles", mchipoPath = OutPutFolder + "mchipo";
+    TString reconhipoPath = OutPutFolder + "reconhipo", rootfilesPath = OutPutFolder + "rootfiles";
+    string lundPath0(lundPath.Data()), mchipoPath0(mchipoPath.Data());
+    string reconhipoPath0(reconhipoPath.Data()), rootfilesPath0(rootfilesPath.Data());
+    DisplyText("lundPath", DisplaySpace, lundPath);
+    DisplyText("mchipoPath", DisplaySpace, mchipoPath);
+    DisplyText("reconhipoPath", DisplaySpace, reconhipoPath);
+    DisplyText("rootfilesPath", DisplaySpace, rootfilesPath);
+
+    TString MonitoringPlotsPath = OutPutFolder + "MonitoringPlotsPath/";
+    string MonitoringPlotsPath0(MonitoringPlotsPath.Data());
+    DisplyText("MonitoringPlotsPath", DisplaySpace, MonitoringPlotsPath);
+
+    TList *plots = new TList();
+    string OutputFileNamePrefix0(OutputFileNamePrefix.Data());
+    string listName = OutPutFolder0 + OutputFileNamePrefix0 + "_plots.root";
+    const char *TListName = listName.c_str();
+    DisplyText("Plot list path", DisplaySpace, listName);
+
+    cout << "\n";
+
+    // Not useful usually
+    double targP = 0.; // polarization
+    double beamP = 0.; // polarization
+    int interactN = 1;
+    int beamType = 11;
+    DisplyText("targP", DisplaySpace, targP);
+    DisplyText("beamP", DisplaySpace, beamP);
+    DisplyText("interactN", DisplaySpace, interactN);
+    DisplyText("beamType", DisplaySpace, beamType);
+
+    double beamE_in_lundfiles = -99; // GeV
+    DisplyText("beamE_in_lundfiles", DisplaySpace, beamE_in_lundfiles);
+
+    double weight = 1;
+    DisplyText("weight", DisplaySpace, weight);
+
+    cout << "\nCreating plot directories...\n";
+
+    system(("rm -r " + OutPutFolder0).c_str());    // Delete old output folder
+    system(("mkdir -p " + OutPutFolder0).c_str()); // Make new output folder
+
+    system(("mkdir -p " + lundPath0).c_str());      // Make new lundfiles folder
+    system(("mkdir -p " + mchipoPath0).c_str());    // Make new mchipo folder
+    system(("mkdir -p " + reconhipoPath0).c_str()); // Make new reconhipo folder
+    system(("mkdir -p " + rootfilesPath0).c_str()); // Make new rootfiles folder
+
+    system(("mkdir -p " + MonitoringPlotsPath0).c_str()); // Make new monitoring plots folder
+
+    /* Add particles in event below*/
+    TVector3 vtx(0, 0, -3); // center of hallB in GEMC in cm (accordig to the targets.h file from the RG-M repository)
+
+    if (GenerateLundFiles)
+    {
+        InitHistograms(gen_1e_events, gen_ep_events, gen_en_events, Ebeam);
+
+        // if (nFiles > nEvents / 10000) { nFiles = nEvents / 10000; }
+
+        cout << "\nGenerating lund files...\n\n";
+
+        for (int iFiles = 1; iFiles < nFiles + 1; iFiles++)
+        {
+            TString OutFileName = Form("%s/%s_%d.txt", lundPath.Data(), OutputFileNamePrefix.Data(), iFiles);
+            cout << "OutFileName: " << std::setw(49) << OutFileName << "\n";
+
+            ofstream OutFile;
+            OutFile.open(OutFileName);
+            if (!OutFile.is_open())
+            {
+                cout << "Output file cannot be created" << endl;
+            }
+
+            TString formatstring, outstring;
+
+            Generate_uniform_event_e_tester(vtx, TH1_hist_list_1e, TH2_hist_list_1e, OutFile, formatstring, outstring, ran, nEvents, targP, beamP, interactN, beamType,
+                                            beamE_in_lundfiles, Ebeam, weight, mass_e, theta_e_min, theta_e_max);
+
+            OutFile.close();
+        }
+
+        cout << "\nPlotting and saving monitoring plots\n\n";
+
+        TCanvas *c1 = new TCanvas("canvas", "canvas", 1000, 750); // normal res
+        c1->SetGrid(), c1->SetBottomMargin(0.14), c1->SetLeftMargin(0.16), c1->SetRightMargin(0.12), c1->cd();
+
+        int num = 0;
+
+        for (int i = 0; i < TH1_hist_list_1e.size(); i++)
+        {
+            TH1_hist_list_1e[i]->Sumw2();
+            TH1_hist_list_1e[i]->GetXaxis()->CenterTitle();
+            TH1_hist_list_1e[i]->GetXaxis()->SetTitleSize(0.06);
+            TH1_hist_list_1e[i]->GetXaxis()->SetLabelSize(0.0425);
+            TH1_hist_list_1e[i]->GetYaxis()->SetTitle("Number of events");
+            TH1_hist_list_1e[i]->GetYaxis()->CenterTitle();
+            TH1_hist_list_1e[i]->GetYaxis()->SetTitleSize(0.06);
+            TH1_hist_list_1e[i]->GetYaxis()->SetLabelSize(0.0425);
+            plots->Add(TH1_hist_list_1e[i]);
+
+            TH1_hist_list_1e[i]->Draw();
+            string SavePath = MonitoringPlotsPath0 + to_string(num + 1) + "_" + TH1_hist_list_1e[i]->GetName() + ".png";
+            c1->SaveAs(SavePath.c_str());
+            ++num;
+        }
+
+        for (int i = 0; i < TH2_hist_list_1e.size(); i++)
+        {
+            TH2_hist_list_1e[i]->GetXaxis()->CenterTitle();
+            TH2_hist_list_1e[i]->GetXaxis()->SetTitleSize(0.06);
+            TH2_hist_list_1e[i]->GetXaxis()->SetLabelSize(0.0425);
+            TH2_hist_list_1e[i]->GetYaxis()->CenterTitle();
+            TH2_hist_list_1e[i]->GetYaxis()->SetTitleSize(0.06);
+            TH2_hist_list_1e[i]->GetYaxis()->SetLabelSize(0.0425);
+            plots->Add(TH2_hist_list_1e[i]);
+
+            TH2_hist_list_1e[i]->Draw("colz");
+            string SavePath = MonitoringPlotsPath0 + to_string(num + 1) + "_" + TH2_hist_list_1e[i]->GetName() + ".png";
+            c1->SaveAs(SavePath.c_str());
+            ++num;
+        }
+
+        TFile *plots_fout = new TFile(TListName, "recreate");
+        plots_fout->cd();
+        plots->Write();
+        plots_fout->Write();
+        plots_fout->Close();
+    }
+    else
+    {
+        cout << "\nLund files generation disabled.\n";
+    }
+}
