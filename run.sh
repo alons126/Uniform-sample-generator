1746e8c2963ff8eb8e0c2af8b69147c1798cc197
alons126 <alonsportes@mail.tau.ac.il>
2024-10-08 11:54:20 +0300

    mid day backup

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-echo "- Re-pulling repository -----------------------------------------------"
-echo ""
-git reset --hard
-git pull
-echo ""
+if [ "$CODE_PATH" != "/Users/alon/Projects/Uniform-sample-generator" ]; then
+    echo "- Re-pulling repository -----------------------------------------------"
+    echo ""
+    unset CODE_PATH
+    export CODE_PATH=$(pwd)
+    git reset --hard
+    git pull
+    echo ""
+fi
+
 echo "- Lunching code -------------------------------------------------------"
 echo ""
 root ./CodeRun.cpp -l -q -b
